509e949cbc1572445b643db690165e67050b8179
overpeak1976-crypto <overpeak1976@gmail.com>
2026-02-03 18:56:05 +0700

    update

--- week_4/tailwind.config.js
+++ week_4/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./app/**/*.{js,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      primary: "#4F46E5", //กำหนดสี
+      secondary: "#9333EA",
+      danger: "#EF4444",
+    },
+    borderRadius: {
+      xl: "1rem", //กำหนดค่า border riadius ใหม่
+    },
+    fontSize: {
+      "10xl": "136px", //กำหนดขนาดอักษร
+    },
   },
   plugins: [],
 }
