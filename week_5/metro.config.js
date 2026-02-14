1e151d695ac75e96d70567d61628dd92759a9960
overpeak1976-crypto <overpeak1976@gmail.com>
2026-02-15 03:22:40 +0700

    s

--- week_5/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-confuig");
-const { withNativewind } = require('nativewind/metro');
-
-const config = getDefaultConfig(__dirname)
-
-module.exports = withNativewind(config, {input: './app/global.css'})
